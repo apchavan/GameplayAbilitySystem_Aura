b5c7224a2e982c0a2aa58f3608b73a5000da3a49
Amey Chavan <ameyc96@gmail.com>
2023-11-26 20:42:43 +0530

    Game Mode

---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright - Amey Chavan
+
+
+#include "Game/AuraGameModeBase.h"
+
--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -13,5 +13,7 @@ UCLASS()
 class AURA_API AAuraCharacter : public AAuraCharacterBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	AAuraCharacter();
 };
