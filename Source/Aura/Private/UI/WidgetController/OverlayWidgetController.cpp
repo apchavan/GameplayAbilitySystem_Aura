d8ceeb5ce605ec1043ebd1bd61014410b02fc0d4
Amey Chavan <ameyc96@gmail.com>
2024-08-19 19:18:40 +0530

    Retrieving Rows from Data Tables

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -37,12 +37,14 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 	).AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
 
 	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
-		[](const FGameplayTagContainer& AssetTags)
+		[this](const FGameplayTagContainer& AssetTags)
 		{
 			for (const FGameplayTag& Tag : AssetTags)
 			{
 				const FString Msg = FString::Printf(TEXT("GE Tag : %s"), *Tag.ToString());
 				GEngine->AddOnScreenDebugMessage(INDEX_NONE, 8.f, FColor::Blue, Msg);
+
+				FUIWidgetRow* Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable, Tag);
 			}
 		}
 	);
