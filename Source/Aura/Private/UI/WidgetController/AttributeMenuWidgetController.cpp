8f84e365e70e60ea8bb60eb8fec5d9d3cd8242fb
Amey Chavan <ameyc96@gmail.com>
2024-09-22 11:30:25 +0530

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Copyright - Amey Chavan
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
