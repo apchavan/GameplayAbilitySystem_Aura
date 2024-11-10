77cac9dc30f4722817a4eb576f098ff7e5819034
Amey Chavan <ameyc96@gmail.com>
2024-11-10 17:01:26 +0530

    Character Class Info

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright - Amey Chavan
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
