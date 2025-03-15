4fea564ebc3ef2d0c1cc40a9734f021720150314
Amey Chavan <ameyc96@gmail.com>
2025-03-15 17:17:21 +0530

    Playing the Ranged Attack Montage
    Since the 'Assets' directory is ignored, the following files are not committed/pushed:
    
    [1] Content/Assets/Enemies/Goblin/Slingshot/Centered/SK_Slingshot.uasset
    [2] Content/Assets/Enemies/Goblin/Slingshot/Centered/SKM_Slingshot.uasset
    Added a socket named as "PouchSocket" that is used in the Blueprint class, `BP_Goblin_Slingshot`.

--- Source/Aura/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
@@ -21,3 +21,13 @@ void UAuraDamageGameplayAbility::CauseDamage(AActor* TargetActor)
 		UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor)
 	);
 }
+
+FTaggedMontage UAuraDamageGameplayAbility::GetRandomTaggedMontageFromArray(const TArray<FTaggedMontage>& TaggedMontages) const
+{
+	if (TaggedMontages.Num() > 0)
+	{
+		const int32 Selection = FMath::RandRange(0, TaggedMontages.Num() - 1);
+		return TaggedMontages[Selection];
+	}
+	return FTaggedMontage();
+}
