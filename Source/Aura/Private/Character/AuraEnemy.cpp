cf60fc66b48a707fee7f8c8036768bb4fc9103e5
Amey Chavan <ameyc96@gmail.com>
2025-03-24 13:44:58 +0530

    Dead Blackboard Key
    Extra Edits:
    
    [1] Source/Aura/Public/Character/AuraEnemy.h
    In this header file, updated comments indentation that tells about functions overridden from 'Combat Interface'.

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -93,6 +93,12 @@ int32 AAuraEnemy::GetPlayerLevel()
 void AAuraEnemy::Die()
 {
 	SetLifeSpan(LifeSpan);
+
+	if (AuraAIController)
+	{
+		AuraAIController->GetBlackboardComponent()->SetValueAsBool(FName("Dead"), true);
+	}
+
 	Super::Die();
 }
 
