afa493802fce8bcf8e90cc5b93365ae1f55a043e
Amey Chavan <ameyc96@gmail.com>
2025-04-01 16:55:04 +0530

    Montage and Socket Tags
    Since the 'Assets' directory is ignored, the following files are not committed/pushed:
    
    [1] Content/Assets/Enemies/Ghoul/Animations/AM_Ghoul_Attack_L.uasset
    [2] Content/Assets/Enemies/Ghoul/Animations/AM_Ghoul_Attack_R.uasset
    [3] Content/Assets/Enemies/Goblin/Animations/Slingshot/AM_Attack_Goblin_Slingshot.uasset
    [4] Content/Assets/Enemies/Goblin/Animations/Spear/AM_Attack_GoblinSpear.uasset
    [5] Content/Assets/Enemies/Shaman/Animations/AM_Attack_Shaman.uasset
    Updated the "Event Tag" property for `AN_MontageEvent` notify.
    
    Extra Edits:
    
    [1] Source/AuraEditor.Target.cs
    Set the 'IncludeOrderVersion' property to fix the compilation warning.

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -36,6 +36,7 @@ public:
 	virtual AActor* GetAvatar_Implementation() override;
 	virtual TArray<FTaggedMontage> GetAttackMontages_Implementation() override;
 	virtual UNiagaraSystem* GetBloodEffect_Implementation() override;
+	virtual FTaggedMontage GetTaggedMontageByTag_Implementation(const FGameplayTag& MontageTag) override;
 
 	/** End Combat Interface */
 
--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -21,6 +21,9 @@ struct FTaggedMontage
 	FGameplayTag MontageTag;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTag SocketTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	USoundBase* ImpactSound = nullptr;
 };
 
@@ -69,4 +72,7 @@ public:
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	UNiagaraSystem* GetBloodEffect();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	FTaggedMontage GetTaggedMontageByTag(const FGameplayTag& MontageTag);
 };
