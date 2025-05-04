1ec26b34e5e6ac46d51410d7f9988e638fd1267a
Amey Chavan <ameyc96@gmail.com>
2025-05-04 12:05:15 +0530

    Minion Count

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -37,6 +37,7 @@ public:
 	virtual TArray<FTaggedMontage> GetAttackMontages_Implementation() override;
 	virtual UNiagaraSystem* GetBloodEffect_Implementation() override;
 	virtual FTaggedMontage GetTaggedMontageByTag_Implementation(const FGameplayTag& MontageTag) override;
+	virtual int32 GetMinionCount_Implementation() override;
 
 	/** End Combat Interface */
 
@@ -115,6 +116,10 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Combat")
 	TObjectPtr<USoundBase> DeathSound;
+
+	/* Minions */
+
+	int32 MinionCount = 0;
 
 private:
 
