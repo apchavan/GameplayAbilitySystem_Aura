68b83dda1b7b41f8ba7f086206d759ce7f6ac860
Amey Chavan <ameyc96@gmail.com>
2026-03-03 12:45:48 +0530

    Electrocute Montage
    Since the 'Assets' directory is ignored, the following files are not committed/pushed:
    
    [1] Content/Assets/Characters/Aura/Animations/Abilities/AM_Cast_Electrocute.uasset
    This Animation Montage is used by the Electrocute Gameplay Ability, 'Content/Blueprints/AbilitySystem/Aura/Abilities/Lightning/GA_Electrocute.uasset', as a starting animation for that Gameplay Ability.

--- Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -20,7 +20,7 @@ public:
 	void StoreMouseDataInfo(const FHitResult& HitResult);
 
 	UFUNCTION(BlueprintCallable)
-	void StoreOwnerPlayerController();
+	void StoreOwnerVariables();
 
 protected:
 
@@ -32,4 +32,7 @@ protected:
 
 	UPROPERTY(BlueprintReadWrite, Category = "Beam")
 	TObjectPtr<APlayerController> OwnerPlayerController;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<ACharacter> OwnerCharacter;
 };
