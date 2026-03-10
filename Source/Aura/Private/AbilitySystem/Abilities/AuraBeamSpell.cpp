76c70e2ca300e0aac7a76db122e8f3eaab16f470
Amey Chavan <ameyc96@gmail.com>
2026-03-10 18:57:44 +0530

    First Trace Target
    Following Changes:
    
    [1] Content/Blueprints/Character/Aura/BP_AuraCharacter.uasset
    Ignored the "Object Responses" for the "Target" (under "Collision Responses") for both `CapsuleComponent` and the character's `Mesh` to the avoid possibility of Aura Electrocuting herself (and other connected players).

--- Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -4,6 +4,7 @@
 #include "AbilitySystem/Abilities/AuraBeamSpell.h"
 
 #include "GameFramework/Character.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
 {
@@ -48,5 +49,42 @@ void UAuraBeamSpell::StoreOwnerVariables()
 	{
 		OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
 		OwnerCharacter = Cast<ACharacter>(CurrentActorInfo->AvatarActor);
+	}
+}
+
+void UAuraBeamSpell::TraceFirstTarget(const FVector& BeamTargetLocation)
+{
+	check(OwnerCharacter);
+
+	if (OwnerCharacter->Implements<UCombatInterface>())
+	{
+		if (USkeletalMeshComponent* Weapon = ICombatInterface::Execute_GetWeapon(OwnerCharacter))
+		{
+			TArray<AActor*> ActorsToIgnore;
+			ActorsToIgnore.Add(OwnerCharacter);
+
+			FHitResult HitResult;
+
+			const FVector SocketLocation = Weapon->GetSocketLocation(FName("TipSocket"));
+
+			UKismetSystemLibrary::SphereTraceSingle(
+				OwnerCharacter,
+				SocketLocation,
+				BeamTargetLocation,
+				10.0f,
+				TraceTypeQuery1,
+				false,
+				ActorsToIgnore,
+				EDrawDebugTrace::None,
+				HitResult,
+				true
+			);
+
+			if (HitResult.bBlockingHit)
+			{
+				MouseHitLocation = HitResult.ImpactPoint;
+				MouseHitActor = HitResult.GetActor();
+			}
+		}
 	}
 }
--- Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -22,6 +22,9 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void StoreOwnerVariables();
 
+	UFUNCTION(BlueprintCallable)
+	void TraceFirstTarget(const FVector& BeamTargetLocation);
+
 protected:
 
 	UPROPERTY(BlueprintReadWrite, Category = "Beam")
